de76e8b1e15f7cad2c5cff4897016525c9892bcd
Vincent Le Badezet <vincent@lajavaness.com>
2022-06-24 10:18:50 +0200

    fix: Delete extra extra content

--- app/root.tsx
+++ app/root.tsx
@@ -1,25 +1,5 @@
-import type { LinksFunction, LoaderFunction, MetaFunction } from '@remix-run/node'
-import { useState } from 'react'
-import { json } from '@remix-run/node'
-import { Form, Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
-import {
-	AppShell,
-	Avatar,
-	Burger,
-	Button,
-	Group,
-	Header,
-	MediaQuery,
-	Navbar,
-	Title,
-	useMantineTheme,
-} from '@mantine/core'
-import { useLocation } from 'react-router-dom'
-import { ListNumbers, Settings, User } from 'tabler-icons-react'
-
-import { getUser } from './session.server'
-
-import NavItem from '~/components/layout/NavItem'
+import type { LinksFunction, MetaFunction } from '@remix-run/node'
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
 
 export const meta: MetaFunction = () => ({
 	charset: 'utf-8',
@@ -36,25 +16,7 @@ export const links: LinksFunction = () => {
 	]
 }
 
-type LoaderData = {
-	user: Awaited<ReturnType<typeof getUser>>
-}
-
-export const loader: LoaderFunction = async ({ request }) => {
-	return json<LoaderData>({
-		user: await getUser(request),
-	})
-}
-
 export default () => {
-	const location = useLocation()
-
-	const theme = useMantineTheme()
-
-	const [opened, setOpened] = useState(false)
-
-	const showNav = location.pathname !== '/' && location.pathname !== '/join' && location.pathname !== '/login'
-
 	return (
 		<html lang="en">
 			<head>
@@ -62,56 +24,7 @@ export default () => {
 				<Links />
 			</head>
 			<body>
-				<AppShell
-					styles={{
-						main: {
-							background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
-						},
-					}}
-					navbarOffsetBreakpoint="sm"
-					asideOffsetBreakpoint="sm"
-					fixed
-					navbar={
-						showNav ? (
-							<Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
-								<Navbar.Section>
-									<NavItem label="Simulations" icon={<ListNumbers />} to="/simulations" />
-									<NavItem label="Settings" icon={<Settings />} to="/settings" />
-								</Navbar.Section>
-							</Navbar>
-						) : undefined
-					}
-					header={
-						<Header height={70} p="md">
-							<div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-								{showNav && (
-									<MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-										<Burger
-											opened={opened}
-											onClick={() => setOpened((o) => !o)}
-											size="xs"
-											color={theme.colors.gray[6]}
-											mr="xl"
-										/>
-									</MediaQuery>
-								)}
-								<Group position="apart" style={{ width: '100%' }}>
-									<Title>Teuh</Title>
-									<Group>
-										<Form action="/logout" method="post">
-											<Button type="submit">Logout</Button>
-										</Form>
-										<Avatar src={null} color="blue" radius="xl">
-											<User />
-										</Avatar>
-									</Group>
-								</Group>
-							</div>
-						</Header>
-					}
-				>
-					<Outlet />
-				</AppShell>
+				<Outlet />
 				<ScrollRestoration />
 				<Scripts />
 				<LiveReload />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,25 +1,3 @@
-import { Anchor, Button, Card, Container, Image, Space, Title } from '@mantine/core'
-import { Link } from 'react-router-dom'
-
 export default function Index() {
-	return (
-		<Container>
-			<Card shadow="sm" p="lg">
-				<Card.Section>
-					<Image
-						src="https://images.unsplash.com/photo-1551801691-f0bce83d4f68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"
-						height={460}
-						alt="Norway"
-					/>
-				</Card.Section>
-				<Space h="xl" />
-				<Title mb="xl" order={3}>
-					Welcome to Teuh
-				</Title>
-				<Anchor component={Link} to="/simulations" variant="text">
-					<Button>View simulations</Button>
-				</Anchor>
-			</Card>
-		</Container>
-	)
+	return <div>Home</div>
 }
